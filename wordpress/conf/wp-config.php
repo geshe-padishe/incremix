0659037afdc26aeaee4b7880f882b9971d08a045
nikotikcho <nikotikcho17@gmail.com>
2024-11-14 16:48:29 +0100

    paresseux

--- wordpress/conf/wp-config.php
+++ wordpress/conf/wp-config.php
@@ -3,5 +3,5 @@ define('DB_NAME', ${DB_NAME});
 define('DB_USER', ${DB_USER});
 define('DB_PASSWORD', ${DB_PASSWORD});
 define('DB_HOST', ${DB_HOST});
-define('WP_CONTENT_DIR', '/var/lib/wordpress/wp-content');
+#define('WP_CONTENT_DIR', '/var/lib/wordpress/wp-content');
 ?>
